c94a81fb4a15a9946cfaf8d54db9e4431d20aa30
Annie Ehler <annie.ehler.4@gmail.com>
2024-05-29 16:00:42 -0700

    Fix locality

--- init.lua
+++ init.lua
@@ -18,6 +18,21 @@ else
     set modelines=5
   ]]
 
+  function VimtexPDFToggle()
+    if vim.g.term_pdf_vierer_open then
+      vim.fn.system("kitty @ close-window --match title:termpdf")
+      vim.g.term_pdf_vierer_open = false
+    elseif vim.g.tex_compiles_successfully then
+      vim.fn.system("kitty @ launch --location=vsplit --cwd=current --title=termpdf")
+
+      local command = "conda activate neovim && termpdf.py " ..
+          vim.api.nvim_call_function("expand", { "%:r" }) .. ".pdf" .. "'\r'"
+      local kitty = "kitty @ send-text --match title:termpdf "
+      vim.fn.system(kitty .. command)
+      vim.g.term_pdf_vierer_open = true
+    end
+  end
+
   -- Set the theme
   vim.cmd.colorscheme(name)
 end
