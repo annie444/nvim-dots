ba7a09fef7675214654ee430a325af5dcf1eda75
Annie Ehler <annie.ehler.4@gmail.com>
2025-01-08 19:37:51 -0800

    Update spectre config

--- lua/plugins/spectre.lua
+++ lua/plugins/spectre.lua
@@ -1,7 +1,13 @@
 return {
   "nvim-pack/nvim-spectre",
   keys = {
-    { '<F4>', "<cmd>SpectreWithCWD<cr>", mode = { 'n' } },
+    {
+      '<F4>',
+      function()
+        require("spectre").toggle()
+      end,
+      mode = { 'n' }
+    },
   },
   config = function()
     require('spectre').setup({ is_block_ui_break = true })
