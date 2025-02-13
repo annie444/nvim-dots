4150da4c384dabc76055aeacc94b406ed9580ebf
Annie Ehler <annie.ehler.4@gmail.com>
2025-02-12 20:44:54 -0800

    [2025-02-12 20:44:54] (update neovim) Committing all changes to nvim-dots

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
-
-
 -- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
 -- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
 local lazypath = vim.env.LAZY or vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -8,6 +6,9 @@ if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
     lazypath })
 end
+
+require("options")
+
 vim.opt.rtp:prepend(lazypath)
 
 -- validate that lazy is available
@@ -16,10 +17,9 @@ if not pcall(require, "lazy") then
   vim.api.nvim_echo(
     { { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } },
     true, {})
-	vim.fn.getchar()
-	vim.cmd.quit()
+  vim.fn.getchar()
+  vim.cmd.quit()
 end
 
-require("options")
 require("lazy_setup")
 require("polish")
