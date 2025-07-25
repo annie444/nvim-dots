cd3716f1cb3f25541ee8aed90d6ed26ec2028078
Annie Ehler <annie.ehler.4@gmail.com>
2025-07-25 11:44:01 -0700

    [2025-07-25 11:44:01] (update neovim) Committing all changes to nvim-dots

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -15,7 +15,31 @@ require("lazy").setup({
   { import = "plugins" },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
-  install = { colorscheme = { "dracula" } },
+  lockfile = vim.fn.stdpath("config") .. "/lazy-lock.json",
+  pkg = {
+    enabled = true,
+    cache = vim.fn.stdpath("state") .. "/lazy/pkg-cache.lua",
+    -- the first package source that is found for a plugin will be used.
+    sources = {
+      "lazy",
+      "rockspec", -- will only be used when rocks.enabled is true
+      "packspec",
+    },
+  },
+  rocks = {
+    enabled = true,
+    root = vim.fn.stdpath("data") .. "/lazy-rocks",
+    server = "https://nvim-neorocks.github.io/rocks-binaries/",
+    -- use hererocks to install luarocks?
+    -- set to `nil` to use hererocks when luarocks is not found
+    -- set to `true` to always use hererocks
+    -- set to `false` to always use luarocks
+    hererocks = true,
+  },
+  install = { 
+    missing = true,
+    colorscheme = { "dracula" }, 
+  },
   ui = { backdrop = 100 },
   performance = {
     rtp = {
