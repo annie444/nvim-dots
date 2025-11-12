a6e79d1c5f0b0cf92ed132589af3b50a24b168af
Annie Ehler <annie.ehler.4@gmail.com>
2025-11-11 21:40:56 -0800

    [2025-11-11 21:40:55] (update neovim) Committing all changes to nvim-dots

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -9,6 +9,53 @@ return {
     opts = {
       dashboard = {
         preset = {
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            {
+              icon = " ",
+              key = "G",
+              desc = "Lazygit",
+              action = function()
+                local astro = require "astrocore"
+                local worktree = astro.file_worktree()
+                local flags = worktree and (" --work-tree=%s --git-dir=%s"):format(worktree.toplevel, worktree.gitdir)
+                  or ""
+                astro.toggle_term_cmd { cmd = "lazygit " .. flags, direction = "float" }
+              end,
+            },
+            {
+              icon = " ",
+              key = "D",
+              desc = "Disk Usage",
+              action = function()
+                local astro = require "astrocore"
+                local gdu = "gdu"
+                if vim.fn.executable(gdu) ~= 1 then
+                  if vim.fn.has "win32" == 1 then
+                    gdu = "gdu_windows_amd64.exe"
+                  elseif vim.fn.has "mac" == 1 then
+                    gdu = "gdu-go"
+                  end
+                end
+                astro.toggle_term_cmd { cmd = gdu, direction = "float" }
+              end,
+            },
+            {
+              icon = " ",
+              key = "B",
+              desc = "Resouce Usage",
+              action = function()
+                local astro = require "astrocore"
+                astro.toggle_term_cmd { cmd = "btm", direction = "float" }
+              end,
+            },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
           header = [[
                                                     
  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ 
@@ -28,7 +75,7 @@ return {
             height = 5,
             padding = 1,
           },
-          { pane = 1, icon = "󰘳", title = "Shortcuts", section = "keys", gap = 1, padding = 1 },
+          { pane = 1, section = "keys", gap = 1, padding = 1 },
           { pane = 1, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
           { pane = 1, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
           {
